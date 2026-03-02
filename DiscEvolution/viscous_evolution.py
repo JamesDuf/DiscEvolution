c0404d3c967491270d7e4a8b6300310d7fcf7996
JamesDuf <139725130+JamesDuf@users.noreply.github.com>
2026-03-02 15:33:36 -0500

    Implemented Michael's bug fix

--- DiscEvolution/viscous_evolution.py
+++ DiscEvolution/viscous_evolution.py
@@ -282,7 +282,7 @@ class ViscousEvolutionFV(object):
 
         if S is None:
             S = disc.Sigma 
-        return - 0.5 * self._dS[1:-1] / (S[1:] + S[:-1])
+        return - 2.0 * self._dS[1:-1] / (S[1:] + S[:-1])
 
     def max_timestep(self, disc):
         """Courant limited time-step"""
@@ -447,7 +447,7 @@ class HybridWindModel(object):
 
         if S is None:
             S = disc.Sigma 
-        return - 0.5 * (self._f_visc + self._f_wind)[1:-1] / (S[1:] + S[:-1])
+        return - 2.0 * (self._f_visc + self._f_wind)[1:-1] / (S[1:] + S[:-1])
 
     def _fluxes(self):
         """Compute the mass fluxes and loss term """
